b794c3c15293eebe5c46b8e381dd10f672b30039
Lucas Lino <lucasperes1591@gmail.com>
2026-03-31 22:14:27 -0300

    feat: Upgrading jobs

--- src/spark/jobs/spark_job_transform_bronze_layer.py
+++ src/spark/jobs/spark_job_transform_bronze_layer.py
@@ -8,7 +8,7 @@ from minio import Minio
 from dotenv import load_dotenv
 
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql import functions as F
+from pyspark.sql import functions as F 
 from pyspark.sql.types import StructType, StructField, StringType
 
 from pyspark.sql.utils import AnalysisException
